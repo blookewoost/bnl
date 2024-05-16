ed762174b3bfa1461b3270445e8f455c001a544e
blookewoost <blakewest1005@gmail.com>
2024-05-16 12:34:50 -0400

    adding read function for RawSocket

--- RawSocket.cpp
+++ RawSocket.cpp
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <cstddef>
+#include <vector>
 
 // Figure out if we can remove this empty constructor.
 RawSocket::RawSocket() {
@@ -15,15 +17,14 @@ RawSocket::RawSocket() {
 // Create a raw socket and bind it to the specified interface.
 RawSocket::RawSocket(const char* interface) {
 
-    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock<0) {
-        printf("Socket creation failed with error %d", errno);
+        printf("Socket creation failed with error %d\n", errno);
         //figure out how to return an error object
     }
 
     unsigned char buf[1024];
-    struct sockaddr saddr;
-    int s_addr_len = sizeof(saddr);
+    s_addr_len = sizeof(saddr);
 
     // Specify the adapter we want to listen on
     struct ifreq ifr;
@@ -32,7 +33,20 @@ RawSocket::RawSocket(const char* interface) {
 
     // Bind socket to a specific network interface
     if (setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
-        printf("Binding to interface: %s failed with error %d", interface, errno);
+        printf("Binding to interface: %s failed with error %d\n", interface, errno);
     }
 
+}
+
+// Read the specified number of bytes from the wire.
+std::vector<char> RawSocket::read(std::size_t buffer_size) {
+
+    std::vector<char> buf(buffer_size);
+    int read = recvfrom(sock, buf.data(), buffer_size, 0, &saddr, (socklen_t *)&s_addr_len);
+
+    if (read<0) {
+        printf("Call to recvfrom failed with error %d\n", errno);
+    } else {
+        return buf;
+    }
 }
--- RawSocket.h
+++ RawSocket.h
@@ -2,4 +2,9 @@ class RawSocket{
     public:
         RawSocket();
         RawSocket(const char* interface);
+        int sock;
+        struct sockaddr saddr;
+        int s_addr_len;
+        std::vector<char> RawSocket::read(std::size_t buffer_size);
+        
 };
