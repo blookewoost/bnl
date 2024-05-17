61e42bff8793fe6b093747c65329e1f6e95d1904
blookewoost <blakewest1005@gmail.com>
2024-05-17 12:41:21 -0400

    build up UDPSender

--- udp/UDPSender.cpp
+++ udp/UDPSender.cpp
@@ -1,10 +1,24 @@
 #include "UDPSender.h"
 #include <string>
+#include <sys/socket.h>
 
 UDPSender::UDPSender() {
 
 }
 
-UDPSender::UDPSender(std::string ip, int port) {
+UDPSender::UDPSender(std::string& ip, int port) {
+
+    // Create a UDP socket
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock<0) {
+        printf("Socket creation failed with error: %d", errno);
+    }
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = port;
+    if (inet_pton(AF_INET, ip.c_str(), &saddr.sin_addr) < 0) {
+        printf("An invalid IP address was provided! inet_pton failed.");
+    }
+
     
 }
--- udp/UDPSender.h
+++ udp/UDPSender.h
@@ -1,5 +1,13 @@
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 class UDPSender{
     public:
         UDPSender::UDPSender();
-        UDPSender::UDPSender(std::string ip, int port);
-};
+        UDPSender::UDPSender(std::string& ip, int port);
+    private:
+        int sock;
+        struct sockaddr_in saddr;
+        int s_addr_len;
+};  
