5cad0b3110601a2abed1835416f5f6d8d134cb65
blookewoost <blakewest1005@gmail.com>
2024-05-20 14:42:40 -0400

    building up tcp client

--- tcp/TCPClient.cpp
+++ tcp/TCPClient.cpp
@@ -1,6 +1,30 @@
 #include "TCPClient.h"
 
-TCPClient::TCPClient() {
+TCPClient::TCPClient(std::string& ip, int port) {
 
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock<0) {
+        printf("Socket creation failed with error: %d", errno);
+    }
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, ip.c_str(), &saddr.sin_addr) <= 0) {
+        printf("%d is an invalid IP address.", ip);
+    }
+
+    s_addr_len = sizeof(saddr);
 };
+
+bool TCPClient::connectToListener() {
+    if (connect(sock, (struct sockaddr*)&saddr, s_addr_len) < 0) {
+        printf("An error occured while trying to connect to TCP Listener!");
+        return false;
+    } else {
+        return true;
+    }
+
+}
 
--- tcp/TCPClient.h
+++ tcp/TCPClient.h
@@ -1,6 +1,13 @@
+#include <string>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
 class TCPClient{
     private:
         int sock;
+        struct sockaddr_in saddr;
+        int s_addr_len;
     public:
-        TCPClient();
+        TCPClient(std::string& ip, int port);
+        bool connectToListener();
 };
