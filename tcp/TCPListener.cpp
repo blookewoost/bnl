96bbc0ea6ae9537ab7d671e255f7d17ccdcd2cb1
blookewoost <blakewest1005@gmail.com>
2024-05-22 13:12:39 -0400

    add TCPListener constructor

--- tcp/TCPListener.cpp
+++ tcp/TCPListener.cpp
@@ -1,5 +1,21 @@
 #include "TCPListener.h"
 
-TCPListener::TCPListener() {
+TCPListener::TCPListener(int port) {
 
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sock<0) {
+        printf("An error occured while creating the socket!");
+    }
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = INADDR_ANY;
+    saddr.sin_port = htons(port);
+
+    s_addr_len = sizeof(saddr);
+
+    if (bind(sock, (struct sockaddr*)&saddr, s_addr_len) < 0) {
+        printf("Binding the socket to saddr failed!");
+    }
 };
+
--- tcp/TCPListener.h
+++ tcp/TCPListener.h
@@ -1,6 +1,13 @@
+#include <string>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <vector>
+
 class TCPListener{
     private:
         int sock;
+        struct sockaddr_in saddr;
+        int s_addr_len;
     public:
-        TCPListener();
+        TCPListener(int port);
 };
