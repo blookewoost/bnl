c1afba4206f306826db2506a14026b9731d8567e
blookewoost <blakewest1005@gmail.com>
2024-05-24 13:00:35 -0400

    build up rs test with udp over loopback

--- raw/tests/test_RawSocket.cpp
+++ raw/tests/test_RawSocket.cpp
@@ -1,12 +1,53 @@
 #include "../RawSocket.h"
 #include <vector>
 #include <cstdio>
+#include <thread>
+#include <chrono>
+#include "../../udp/UDPReceiver.h"
+#include "../../udp/UDPSender.h"
 
-int main() {
-    // create raw socket on the loopback adapter.
-    RawSocket socket = RawSocket("wlp0s20f3");
-    std::vector<char> buf = socket.read(1024);
-    printf(buf.data());
+// Let's send a UDP packet and snoop on it with our RawSocket
+void send_udp() {
+
+    std::string ip = "127.0.0.1";
+    UDPSender sender = UDPSender(ip, 9000);
+
+    std::string message = "Hello from UDP sender!";
+    std::vector<char> buffer;
+    buffer.assign(message.begin(), message.end());
+
+    if (sender.send_bytes(buffer)) {
+        
+    }
 }
 
-void send_udp_data();
+void receive_udp() {
+
+    UDPReceiver receiver = UDPReceiver(9000);
+    ssize_t buffersize = 1024;
+    std::vector<char> buffer = receiver.read(buffersize);
+    std::string message(buffer.begin(), buffer.end());
+    printf("Received message!: %s", message.c_str());
+}
+
+void snoop_with_rs() {
+    RawSocket rs = RawSocket("lo");
+    std::vector<char> buf = rs.read(1024);
+    // chop up the packet here to see contents.
+}
+
+int main() {
+
+
+    // Declare threads.
+    std::thread rs_thread(snoop_with_rs);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::thread receiver_thread(receive_udp);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::thread sender_thread(send_udp);
+    
+    rs_thread.join();
+    receiver_thread.join();
+    sender_thread.join();
+
+}
